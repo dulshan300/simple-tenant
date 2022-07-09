522b2ba553fe1e9566efac15acda008be93eed4d
Dulshan <abavira@gmail.com>
2022-07-09 09:38:21 +0530

    did some changes
    Signed-off-by: Dulshan <abavira@gmail.com>

--- src/Helper/SimpleTenantHelper.php
+++ src/Helper/SimpleTenantHelper.php
@@ -9,33 +9,9 @@ if (!function_exists('makeTenantDB')) {
         $db_name = env('TENANT_PREFIX') . "_" . $id;
         $query = "CREATE DATABASE {$db_name}";
         DB::statement($query);
-
-        setTenant($id);       
-        Artisan::call('migrate');
-        unsetTenant();
     }
 }
 
-if (!function_exists('tenant')) {
-
-    function tenant(Closure $clouser, $tenent = false)
-    {
-        if (auth()->check()) {
-            $user = $tenent ? $tenent : auth()->user();
-
-            $bd_prime = config('database.connections.mysql.database');
-            // change tenant           
-            $db_name = env('TENANT_PREFIX') . "_" . $user->tenant_id;
-            config(['database.connections.mysql.database' => $db_name]);
-            DB::reconnect('mysql');
-            // execute
-            $clouser();
-
-            config(['database.connections.mysql.database' => $bd_prime]);
-            DB::reconnect('mysql');
-        }
-    }
-}
 
 if (!function_exists('setTenant')) {
 
@@ -44,12 +20,13 @@ if (!function_exists('setTenant')) {
         if (auth()->check()) {
             $tenent_id = $tenent_id ? $tenent_id : auth()->user()->tenant_id;
             // change tenant
-            // session(['dbname' => config('database.connections.mysql.database')]);
+            // session(['dbname' => config('database.connections.mysql.database')]);            
             $db_name = env('TENANT_PREFIX') . "_" . $tenent_id;
+            DB::purge();
             config(['database.connections.mysql.database' => $db_name]);
             DB::reconnect('mysql');
         }
-        DB::purge();
+        
     }
 }
 
@@ -57,8 +34,9 @@ if (!function_exists('unsetTenant')) {
 
     function unsetTenant()
     {
+        DB::purge();
         config(['database.connections.mysql.database' => env('DB_DATABASE')]);
         DB::reconnect('mysql');
-        DB::purge();
+        
     }
 }
